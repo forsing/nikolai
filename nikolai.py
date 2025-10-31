f1877e2f72fe5c1a08ed8f35432dc16dac481ea0
ForsIng <forsing@gmail.com>
2025-11-01 00:00:09 +0100

    Update nikolai.py

--- nikolai.py
+++ nikolai.py
@@ -620,6 +620,16 @@ print()
 print("###############################")
 print()
 
+"""
+=== Qiskit Version Table ===
+Software                       Version        
+---------------------------------------------
+qiskit                         1.4.4          
+qiskit_machine_learning        0.8.3          
 
-
+=== System Information ===
+Python version                 3.11.13        
+OS Apple                       Darwin 
+"""
 ################################################
+
